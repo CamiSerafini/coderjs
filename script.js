f2c388f590ddbc8f901c0ddca14baff66b6816e6
Camila Rocio Serafini <camiiserafinii@gmail.com>
2024-12-22 21:15:30 -0300

    Haciendo la actividad practica 1

---
+++ script.js
@@ -0,0 +1,13 @@
+let nombre = "Camila";
+let nombre2;
+nombre2 = "Juan";
+let num1 = 5;
+let verdadero;
+let nulo = null;
+let falso = false;
+console.log(nombre);
+console.log(nombre2);
+console.log(num1);
+console.log(verdadero); //Como no le di valor me da undefined
+console.log(nulo); //Null es un valor
+console.log(falso);
